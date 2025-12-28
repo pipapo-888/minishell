d6fcba50e58c31d55d08ea2cedd0c90e8004807b
Hideumi Abe <habe@c2r9s2.42tokyo.jp>
2025-12-28 19:49:28 +0900

    refac/pipexUsage削除,構造体定義

--- minishell.h
+++ minishell.h
@@ -6,21 +6,43 @@
 /*   By: habe <habe@student.42tokyo.jp>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/28 13:24:27 by habe              #+#    #+#             */
-/*   Updated: 2025/12/28 13:47:58 by habe             ###   ########.fr       */
+/*   Updated: 2025/12/28 19:03:26 by habe             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#include <sys/wait.h>
-#include <readline/readline.h>
-#include <readline/history.h>
-#include "libft/libft.h"
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+# include <unistd.h>
+# include <sys/wait.h>
+# include <readline/readline.h>
+# include <readline/history.h>
+# include "libft/libft.h"
+
+
+typedef struct s_cmd
+{
+	char	**argv;
+	char	*path;
+}	t_cmd;
+
+typedef struct s_env
+{
+	char			*env;
+	struct s_env	*next;
+}	t_env;
+
+typedef struct s_data
+{
+	t_cmd	*cmd;
+	t_env	*env;
+
+}	t_data;
+
+
 
 void	prompt(char **ev);
 int		main(int ac, char **av, char **ev);
