9782ad18b19c45978cc2500d1e3db8926c0492f2
Hideumi Abe <habe@c2r9s2.42tokyo.jp>
2026-01-10 11:09:23 +0900

    feat/built_in_cd実装

--- init.c
+++ init.c
@@ -98,12 +98,54 @@ void	built_in_env(char **ev)
 	return ;
 }
 
+int	argument_count(char **argv)
+{
+	int	count;
+
+	count = 0;
+	while (argv[count] != NULL)
+		count++;
+	return (count);
+}
+
+void	built_in_cd(t_cmd *cmd, char **ev)
+{
+	char	*path;
+
+	if (argument_count(cmd->argv) > 2)
+	{
+		write(2, "minishell: cd: too many arguments\n", 34);
+		return ;
+	}
+	path = NULL;
+	if (cmd->argv[1] == NULL || ft_strcmp(cmd->argv[1], "~") == 0)
+	{
+		path = get_env_value(ev, "HOME");
+		if (path == NULL)
+			return (perror("cd: HOME not set"));
+	}
+	else if (ft_strcmp(cmd->argv[1], "-") == 0)
+	{
+		path = get_env_value(ev, "OLDPWD");
+		if (path == NULL)
+			return (perror("cd: OLDPWD not set"));
+	}
+	if (path != NULL)
+	{
+		if (chdir(path) != 0)
+			return (perror("cd"));
+		return ;
+	}
+	if (chdir(cmd->argv[1]) != 0)
+		return (perror("cd"));
+}
+
 int	built_in_check(t_cmd *cmd, char **ev)
 {
 	if (!ft_strcmp(cmd->argv[0], "echo"))
 		return (built_in_echo(cmd), 0);
 	if (!ft_strcmp(cmd->argv[0], "cd"))
-		return (chdir(cmd->argv[1]), 0);
+		return (built_in_cd(cmd, ev), 0);
 	if (!ft_strcmp(cmd->argv[0], "pwd"))
 		return (built_in_pwd(cmd), 0);
 	if (!ft_strcmp(cmd->argv[0], "export"))
