6bbd4da2ee55b065e497939ba5540cda307f037c
pipapo-888 <kazuya2002uk@gmail.com>
2026-01-14 17:57:38 +0900

    パイプ実行部分

--- execve.c
+++ execve.c
@@ -27,22 +27,76 @@ int	built_in_check(t_cmd *cmd, t_data *data)
 	return (1);
 }
 
-void	ft_execve(t_cmd *cmd, t_data *data, char **env)
+// void	ft_execve(t_cmd *cmd, t_data *data, char **env)
+// {
+// 	pid_t	pid;
+
+// 	if (built_in_check(cmd, data) == 0)
+// 		return ;
+// 	pid = fork();
+// 	if (pid == 0)
+// 	{
+// 		signal(SIGQUIT, SIG_DFL);
+// 		signal(SIGINT, SIG_DFL);
+// 		setup_redirects(cmd);
+// 		execve(cmd->path, cmd->argv, env);
+// 		perror("execve");
+// 		exit(1);
+// 	}
+// 	else
+// 		wait(NULL);
+// }
+
+void ft_execve(t_cmd *cmd, t_data *data, char **env)
 {
+	int		pfd[2];
+	int		prev_fd;
 	pid_t	pid;
 
-	if (built_in_check(cmd, data) == 0)
+	prev_fd = -1;
+	if (cmd->next == NULL && built_in_check(cmd, data) == 0)
 		return ;
-	pid = fork();
-	if (pid == 0)
+	while (cmd != NULL)
 	{
-		signal(SIGQUIT, SIG_DFL);
-		signal(SIGINT, SIG_DFL);
-		setup_redirects(cmd);
-		execve(cmd->path, cmd->argv, env);
-		perror("execve");
-		exit(1);
+		// 次のコマンドがあればパイプ作成
+		if (cmd->next != NULL)
+			pipe(pfd);
+		pid = fork();
+		if (pid == 0)
+		{
+			signal(SIGQUIT, SIG_DFL);
+			signal(SIGINT, SIG_DFL);
+			// 前のパイプから読み取り（最初のコマンド以外）
+			if (prev_fd != -1)
+			{
+				dup2(prev_fd, STDIN_FILENO);
+				close(prev_fd);
+			}
+			// 次のパイプに書き込み（最後のコマンド以外）
+			if (cmd->next != NULL)
+			{
+				close(pfd[0]);
+				dup2(pfd[1], STDOUT_FILENO);
+				close(pfd[1]);
+			}
+			setup_redirects(cmd);
+			if (built_in_check(cmd, data) == 0)
+				exit(0);
+			execve(cmd->path, cmd->argv, env);
+			perror("execve");
+			exit(1);
+		}
+		// 親プロセス: fdを閉じて次へ（waitはまだしない！）
+		if (prev_fd != -1)
+			close(prev_fd);
+		if (cmd->next != NULL)
+		{
+			close(pfd[1]);
+			prev_fd = pfd[0];
+		}
+		cmd = cmd->next;
 	}
-	else
-		wait(NULL);
+	// 全ての子プロセスを待機（ループの外で！）
+	while (wait(NULL) > 0)
+		;
 }
