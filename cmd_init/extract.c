7d1c484aff0f8d942387b723a1ba044e8033f197
Hideumi Abe <habe@c4r1s5.42tokyo.jp>
2026-01-20 16:05:07 +0900

    refac/バグ修正

--- cmd_init/extract.c
+++ cmd_init/extract.c
@@ -28,6 +28,7 @@ int	extract_pipe_token(t_token **token)
 		return (0);
 	(*token)->type = PIPE;
 	(*token)->value = ft_strdup("|");
+	(*token)->quoted = 0;
 	(*token)->next = NULL;
 	return (1);
 }
@@ -39,6 +40,7 @@ int	extract_heredoc(t_token **token)
 		return (0);
 	(*token)->next = NULL;
 	(*token)->type = HEREDOC;
+	(*token)->quoted = 0;
 	(*token)->value = ft_strdup("<<");
 	return (2);
 }
@@ -50,6 +52,7 @@ int	extract_redirect_in(t_token **token)
 		return (0);
 	(*token)->next = NULL;
 	(*token)->type = REDIR_IN;
+	(*token)->quoted = 0;
 	(*token)->value = ft_strdup("<");
 	return (1);
 }
@@ -64,12 +67,14 @@ int	extract_redirect_out(const char *input, t_token **token)
 		if (input[1] == '>')
 		{
 			(*token)->type = REDIR_APPEND;
+			(*token)->quoted = 0;
 			(*token)->value = ft_strdup(">>");
 			return (2);
 		}
 		else
 		{
 			(*token)->type = REDIR_OUT;
+			(*token)->quoted = 0;
 			(*token)->value = ft_strdup(">");
 			return (1);
 		}
@@ -92,6 +97,7 @@ int	extract_word_token(const char *input, t_token **token)
 	if (!*token)
 		return (0);
 	(*token)->type = WORD;
+	(*token)->quoted = 0;
 	(*token)->value = ft_substr(input, 0, end);
 	(*token)->next = NULL;
 	return (end);
