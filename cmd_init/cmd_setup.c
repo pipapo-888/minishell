424cfa256af8bfdceb7adcfc983c8f434264c17f
Hideumi Abe <habe@c5r4s4.42tokyo.jp>
2026-01-31 18:55:42 +0900

    feat/exitStatus=2実装+error出力

--- cmd_init/cmd_setup.c
+++ cmd_init/cmd_setup.c
@@ -1,13 +1,13 @@
 #include "../minishell.h"
 
-void	put_in_path(t_cmd *cmd, char **ev)
+static void	put_in_path(t_cmd *cmd, char **ev)
 {
 	t_cmd	*head;
 
 	head = cmd;
 	while (head != NULL)
 	{
-		if (ft_strchr(head->argv[0], '/') != NULL)
+		if (head->argv != NULL && ft_strchr(head->argv[0], '/') != NULL)
 		{
 			if (access(head->argv[0], X_OK) == 0)
 				head->path = ft_strdup(head->argv[0]);
@@ -18,7 +18,7 @@ void	put_in_path(t_cmd *cmd, char **ev)
 	}
 }
 
-void	expand_tokens(t_token *token, t_env *env)
+static void	expand_tokens(t_token *token, t_env *env)
 {
 	char	*temp_value;
 
@@ -33,6 +33,45 @@ void	expand_tokens(t_token *token, t_env *env)
 	}
 }
 
+static int	is_tokens_type(t_token_type type)
+{
+	if (type == REDIR_IN || type == REDIR_OUT
+		|| type == REDIR_APPEND || type == HEREDOC)
+		return (1);
+	return (0);
+}
+
+static int	is_syntax_error(t_token *tokens)
+{
+	if (tokens == NULL)
+		return (0);
+	if (tokens->type == PIPE)
+	{
+		write(2, "minishell: ", 11);
+		ft_putstr_fd("syntax error near unexpected token `|'\n", 2);
+		return (1);
+	}
+	while (tokens != NULL)
+	{
+		if (is_tokens_type(tokens->type) && \
+			(tokens->next == NULL || tokens->next->type != WORD))
+		{
+			write(2, "minishell: ", 11);
+			ft_putstr_fd("syntax error near unexpected token `newline'\n", 2);
+			return (1);
+		}
+		if (tokens->type == PIPE && \
+			(tokens->next == NULL || tokens->next->type == PIPE))
+		{
+			write(2, "minishell: ", 11);
+			ft_putstr_fd("syntax error near unexpected token `|'\n", 2);
+			return (1);
+		}
+		tokens = tokens->next;
+	}
+	return (0);
+}
+
 void	cmd_setup(t_data *data, t_cmd *cmd, char *input, char **ev)
 {
 	t_token	*tokens;
@@ -40,6 +79,13 @@ void	cmd_setup(t_data *data, t_cmd *cmd, char *input, char **ev)
 	tokens = tokenize(input);
 	if (tokens == NULL)
 		return ;
+	if (is_syntax_error(tokens) != 0)
+	{
+		free_tokens(tokens);
+		data->cmd = NULL;
+		set_exit_status(data->env, SHELL_ERROR);
+		return ;
+	}
 	expand_tokens(tokens, data->env);
 	put_in_cmd(data, cmd, &tokens);
 	free_tokens(tokens);
